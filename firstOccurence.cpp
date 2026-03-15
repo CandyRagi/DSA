564f749a2da1dd50f5af698e71cb8a25ad1b762a
Candy <tiwariansh935@gmail.com>
2026-03-15 00:16:06 -0700

    First Occurence

---
+++ firstOccurence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.length();
+        int m = needle.length();
+
+        if (m == 0) return 0;
+
+        for (int i = 0; i <= n - m; i++) {
+            if (haystack[i] == needle[0]) {
+                bool flag = true;
+     
+                for (int j = 0; j < m; j++) {
+                    if (haystack[i + j] != needle[j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                
+                if (flag) return i;
+            }
+        }
+        return -1;
+    }
+};
